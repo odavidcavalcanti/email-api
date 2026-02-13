507f42359f03c2a279c3c62b89c2b7d88f626896
David Cavalcanti <davidomcavalcanti02@gmail.com>
2026-02-13 18:16:38 -0300

    feat: controller for handling api

---
+++ src/main/java/com/geeklab/email/controller/EmailController.java
@@ -0,0 +1,26 @@
+package com.geeklab.email.controller;
+
+import com.geeklab.email.DTO.ContactRequestDTO;
+import com.geeklab.email.service.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class EmailController {
+    private final EmailService mailService;
+
+    public EmailController(EmailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @PostMapping("/contactus")
+    public ResponseEntity<?> sendMail (@Valid @RequestBody ContactRequestDTO requestDTO) throws Exception{
+        mailService.sendEmails(requestDTO);
+        return ResponseEntity.ok().build();
+    }
+}
